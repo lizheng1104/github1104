ff62b07482747cc469701eda7a9b02a08a340826
LiZheng <johndoe@example.com>
2019-03-08 16:13:50 +0800

    haha

--- scripts/test_contact.py
+++ scripts/test_contact.py
@@ -1,11 +1,10 @@
-
 class TestAbc:
 
     def test_abc1(self):
-         assert  1
+         assert 1
 
     def test_abc2(self):
-         assert  1
+         assert 1
 
     def test_abc3(self):
-        assert 0
+         assert 0
